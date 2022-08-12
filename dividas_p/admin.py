97892028a2c4a3c645865a7a114d2f0bcd831eef
Paulo G G Matias <pgabrieldeveloper@outlook.com>
2022-08-12 12:38:09 -0300

    create migrations pessoa e dividas e adicionando ao admin

--- dividas_p/admin.py
+++ dividas_p/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
-
+from .models import PessoaF, Divida
 # Register your models here.
+
+@admin.register(PessoaF)
+class PessoaFAdmin(admin.ModelAdmin):
+    list_display = [
+        'nome',
+        'idade',
+        'cpf',
+        'ativa',
+        
+    ]
+
+@admin.register(Divida)
+class DividadeAdmin(admin.ModelAdmin):
+    list_display = [
+        'nome',
+        'local',
+        'data',
+        'valor',
+        'status',
+        'pessoas'
+    ]
--- dividas_p/models.py
+++ dividas_p/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class PessoaF(models.Model):
+    nome = models.TextField('Nome')
+    idade = models.IntegerField("IDADE")
+    cpf = models.TextField("CPF")
+    ativa = models.BooleanField('ativa')
+
+    def __str__(self) -> str:
+        return f'Pessoa Fisica: {self.nome}'
+
+class Divida(models.Model):
+    nome = models.TextField('Nome')
+    local = models.TextField('Local')
+    data = models.DateField('Data')
+    valor = models.FloatField('Valor')
+    status = models.BooleanField('Status')
+    pessoas = models.ForeignKey(PessoaF, on_delete=models.PROTECT, related_name='dividas')
+
