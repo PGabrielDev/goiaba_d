392d7b981f87d580572da93a025c65373af2035d
Paulo G G Matias <pgabrieldeveloper@outlook.com>
2022-08-13 16:58:01 -0300

    criando funções de login cadastro e dividas

--- dividas_p/urls.py
+++ dividas_p/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import login
+from .views import login, dividas, cadastro
 
 urlpatterns = [
-    path('login', login, name='login')
+    path('login', login, name='login'),
+    path('dividas', dividas, name='dividas'),
+    path('cadastro', cadastro, name='cadastro' )
 ]
--- dividas_p/views.py
+++ dividas_p/views.py
@@ -1,5 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_d
 
+from dividas_p.models import PessoaF
 # Create your views here.
 def login(request):
-    return render(request, 'login.html')
+    if request.method == "GET":
+        return render(request, 'login.html')
+    user_login = request.POST.get('user_login')
+    senha = request.POST.get('senha')
+    user = authenticate(username=user_login, password=senha)
+    if user:
+        login_d(request, user)
+        return render(request, 'dividas.html')
+
+def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    cpf = request.POST.get('cpf')
+    pessoaF = PessoaF.objects.filter(cpf=cpf)
+
+    if not pessoaF:
+        return redirect('cadastro')
+
+    user_login = request.POST.get('user_login')
+    first_name = request.POST.get('first_name')
+    senha = request.POST.get('senha')
+    user = User.objects.create_user(first_name=first_name, username=user_login, password=senha)
+    user = user.save()
+    user = authenticate(username=user_login, password=senha)
+    if user:
+        login_d(request, user)
+        return render(request, 'dividas.html')
+        
+    else:
+        return render(request, 'login.html')
+
+    
+
+def dividas(request):
+    return render(request, 'dividas.html')
+
